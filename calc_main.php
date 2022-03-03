671a39318d1a97a8adf440e9f0443154d2edc132
mohmedfasil <mohmedfasilp@gmail.com>
2022-03-03 12:33:40 +0530

    Updated

--- calc_main.php
+++ calc_main.php
@@ -16,25 +16,29 @@ require_once "calculator.php";
 echo "The first number is:".$_POST["fnum"]."<br>";
 echo "The second number is:".$_POST["snum"];
 
-$add=new Calculator;
+$fresult=new Calculator;
 
 if (is_numeric($_POST["fnum"])==true && is_numeric($_POST["snum"])==true)
 {
     if (isset($_POST["Addition"]))
     {                
-        $add->Addition($_POST["fnum"],$_POST["snum"]);
+        $a=$fresult->Addition($_POST["fnum"],$_POST["snum"]);
+        echo "<b>The Result of addition is $a</b>";
     }
     elseif (isset($_POST["Substraction"]))
     { 
-        $sub->Substraction($_POST["fnum"],$_POST["snum"]); 
+        $b=$fresult->Substraction($_POST["fnum"],$_POST["snum"]);
+        echo "<b>The Result of Substraction is $b</b>";
     }
     elseif (isset($_POST["Multiplication"]))
     {   
-        $add->Multiplication($_POST["fnum"],$_POST["snum"]);
+        $c=$fresult->Multiplication($_POST["fnum"],$_POST["snum"]);
+        echo "<b>The Result of Multiplication is $c</b>";
     }
     elseif (isset($_POST["Division"]))
     {
-        $add->Division($_POST["fnum"],$_POST["snum"]);
+        $d=$fresult->Division($_POST["fnum"],$_POST["snum"]);
+        echo "<b>The Result of Division is $d</b>";
     }
 }
 else
--- calculator.php
+++ calculator.php
@@ -1,45 +1,37 @@
-<html>
-    <head>
-        <title>Calculator Page</title>    
-    </head>
-    <body>
+<?php
 
-    <?php
+class Calculator
+{
+    public $num1;
+    public $num2;
 
-    class Calculator
+    function Addition($num1,$num2)
     {
-        public $num1;
-        public $num2;
-    
-        function Addition($num1,$num2)
-        {
-            $sum=$num1+$num2;
-            echo "<br><br>";
-            echo "<b>The Result of addition is $sum</b>";
-        }
-        
-        function Substraction($num1,$num2)
-        {
-            echo "<br><br>";
-            $sum=$num1-$num2;
-            echo "<b>The Result of substraction is $sum</b>";
-        }
-            
-        function Multiplication($num1,$num2)
-        {
-            echo "<br><br>";
-            $sum=$num1*$num2;
-            echo "<b>The Result of multiplication is $sum</b>";
-        }
-        
-        function Division($num1,$num2)
-        {
-            echo "<br><br>";
-            $sum=$num1/$num2;
-            echo "<b>The Result of division is $sum</b>";
-        }
+        $result=$num1+$num2;
+        echo "<br><br>";
+        return  $result;
     }
-    
-    ?>
-    </body>
-</html>
+        
+    function Substraction($num1,$num2)
+    {
+        echo "<br><br>";
+        $result=$num1-$num2;
+        return $result;
+    }
+        
+    function Multiplication($num1,$num2)
+    {
+        echo "<br><br>";
+        $result=$num1*$num2;
+        return $result;
+    }
+        
+    function Division($num1,$num2)
+    {
+        echo "<br><br>";
+        $result=$num1/$num2;
+        return $result;
+    }
+}
+
+?>

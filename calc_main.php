f8e43fbd11a43b0d3a6094b76ea0fd1998fd3221
mohmedfasil <mohmedfasilp@gmail.com>
2022-03-03 20:23:37 +0530

    Updated 1

--- calc_main.php
+++ calc_main.php
@@ -13,38 +13,33 @@
 
 require_once "calculator.php";
 
-echo "The first number is:".$_POST["fnum"]."<br>";
-echo "The second number is:".$_POST["snum"];
+$Calculator=new Calculator;
 
-$fresult=new Calculator;
+$a=$_POST["num1"];
+$b=$_POST["num2"];
 
-if (is_numeric($_POST["fnum"])==true && is_numeric($_POST["snum"])==true)
-{
-    if (isset($_POST["Addition"]))
-    {                
-        $a=$fresult->Addition($_POST["fnum"],$_POST["snum"]);
-        echo "<b>The Result of addition is $a</b>";
-    }
-    elseif (isset($_POST["Substraction"]))
-    { 
-        $b=$fresult->Substraction($_POST["fnum"],$_POST["snum"]);
-        echo "<b>The Result of Substraction is $b</b>";
-    }
-    elseif (isset($_POST["Multiplication"]))
-    {   
-        $c=$fresult->Multiplication($_POST["fnum"],$_POST["snum"]);
-        echo "<b>The Result of Multiplication is $c</b>";
-    }
-    elseif (isset($_POST["Division"]))
-    {
-        $d=$fresult->Division($_POST["fnum"],$_POST["snum"]);
-        echo "<b>The Result of Division is $d</b>";
-    }
+echo "The first number is:".$a."<br>";
+echo "The second number is:".$b;
+
+if (isset($_POST["Addition"]))
+{                
+    $result=$Calculator->Addition($a,$b);
+    echo "<br><br><b>The Result of addition is <u> $result </u></b>";
 }
-else
+elseif (isset($_POST["Substraction"]))
+{ 
+    $result=$Calculator->Substraction($a,$b);
+    echo "<br><br><b>The Result of Substraction is <u> $result </u></b>";
+}
+elseif (isset($_POST["Multiplication"]))
+{   
+    $result=$Calculator->Multiplication($a,$b);
+    echo "<br><br><b>The Result of Multiplication is <u> $result </u></b>";
+}
+elseif (isset($_POST["Division"]))
 {
-        echo "<br>";
-        echo "Please enter numeric values only on both fields";
+    $result=$Calculator->Division($a,$b);
+    echo "<br><br><b>The Result of Division is <u> $result </u></b>";
 }
 
 
--- calculator.php
+++ calculator.php
@@ -2,35 +2,38 @@
 
 class Calculator
 {
-    public $num1;
-    public $num2;
+    public $num1,$num2;
 
     function Addition($num1,$num2)
     {
         $result=$num1+$num2;
-        echo "<br><br>";
         return  $result;
     }
         
     function Substraction($num1,$num2)
     {
-        echo "<br><br>";
         $result=$num1-$num2;
         return $result;
     }
         
     function Multiplication($num1,$num2)
     {
-        echo "<br><br>";
         $result=$num1*$num2;
         return $result;
     }
         
     function Division($num1,$num2)
     {
-        echo "<br><br>";
-        $result=$num1/$num2;
-        return $result;
+        if($num2!=0)
+        {
+            $result=$num1/$num2;
+            return $result;
+        }
+        else
+        {
+            $result="Infinite (division by zero)";
+            return $result;
+        }        
     }
 }
 
